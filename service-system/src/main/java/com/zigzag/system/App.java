19b051b9a2eda84bf94fb9801dfe71459b6b40bb
qianlk <740942486@qq.com>
2024-03-30 20:48:45 +0800

    build(pom): 添加依赖

---
+++ service-system/src/main/java/com/zigzag/system/App.java
@@ -0,0 +1,14 @@
+package com.zigzag.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qlk
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
